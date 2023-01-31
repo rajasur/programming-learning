ab9ef5f7b0ab8e1f103a4b5d8a6f5f908941fb3c
Sur <raja.sur@sap.com>
2023-01-31 16:56:01 +0530

    update

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Raja string = "value"
+
+func main() {
+	fmt.Println("variables")
+
+	var smallValue int = 23456
+	fmt.Println(smallValue)
+	fmt.Printf("Type: %T", smallValue)
+
+	//Valueran
+	smallFloat := 2345.118765
+	print(smallFloat)
+	fmt.Printf("Type: %T", smallFloat)
+
+}
