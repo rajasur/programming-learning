01d0d778086a19ad3192f9d0c6efb27f3af2dbaf
I571352 <raja.sur@sap.com>
2025-06-26 15:54:45 +0530

    add calMath class to demonstrate various mathematical functions in Java

---
+++ JAVA/7. Control statement math and string/src/calMath.java
@@ -0,0 +1,23 @@
+public class calMath {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-99));
+        System.out.println(Math.ceil(5.07));
+        System.out.println(Math.floor(5.07));
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.random());
+        System.out.println(Math.round(5.07));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.toDegrees(Math.PI));
+        System.out.println(Math.toRadians(180));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.log(10)); // Natural logarithm
+        System.out.println(Math.log10(100)); // Base 10 logarithm
+        System.out.println(Math.log1p(1)); // Natural logarithm of (1 + x)
+        System.out.println(Math.expm1(1)); // e^x - 1
+        System.out.println(Math.sin(Math.PI / 2)); // Sine of 90 degrees
+        System.out.println(Math.cos(Math.PI)); // Cosine of 180 degrees
+    }
+}
